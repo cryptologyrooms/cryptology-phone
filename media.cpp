ff1d23c01557e6d56b15626405bf95aec564a996
James Fowkes <jamesfowkes@gmail.com>
2019-05-14 16:14:53 +0100

    Add media handling (untested)

--- media.cpp
+++ media.cpp
@@ -1,15 +1,48 @@
 #include <stdint.h>
 
+#include <SoftwareSerial.h>
+
+#include "raat.hpp"
+
 #include "media.h"
+
+/* Local Variables */
+
+static SoftwareSerial s_serialport(MEDIA_RX, MEDIA_TX);
 
 /* Public Functions */
 
+void media_begin()
+{
+	s_serialport.begin(115200);
+}
+
 void media_play(uint8_t track_number)
 {
-	(void)track_number;
+	switch(track_number)
+	{
+	case TRACK_DIALTONE:
+		s_serialport.print("Pdialtone.ogg\n");
+		break;
+	case TRACK_WRONG_NUMBER:
+		s_serialport.print("Punknownnumber.ogg\n");
+		break;
+	case TRACK_CORRECT_NUMBER:
+		s_serialport.print("Pcorrectnumber.ogg\n");
+		break;
+	case TRACK_CUSTOM_1:
+		s_serialport.print("Pcustom1.ogg\n");
+		break;
+	case TRACK_CUSTOM_2:
+		s_serialport.print("Pcustom2.ogg\n");
+		break;
+	case TRACK_CUSTOM_3:
+		s_serialport.print("Pcustom3.ogg\n");
+		break;
+	}
 }
 
 void media_stop()
 {
-
+	s_serialport.print("q\n");
 }
